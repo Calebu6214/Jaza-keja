9f6c6ca79655f312da9ca00c54af32420511c633
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-09-29 00:33:54 +0300

    initializing login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl,FormGroup, Validators} from '@angular/forms';
+import {FormControl} from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs/operators';
+
+import { AccountService, AlertService } from '@app/_services';
 
 @Component({
   selector: 'app-login',
@@ -7,26 +12,54 @@ import {FormControl,FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl!: string;
 
-  constructor() { }
+  constructor(  private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private accountService: AccountService,
+    private alertService: AlertService) { }
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('',[Validators.required, Validators.min(4)]);
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.email.hasError('email') ? 'Not a valid email' : '';
-  }
-  getErrorMess() {
-    if (this.password.hasError('required')) {
-      return 'You must enter a value';
-    }
-
-    return this.password.hasError('password') ? 'Not a valid password' : '';
-  }
+  
   ngOnInit(): void {
+
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+  });
+
+  // get return url from route parameters or default to '/'
+  this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.form.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // reset alerts on submit
+        this.alertService.clear();
+
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        this.accountService.login(this.f.username.value, this.f.password.value)
+            .pipe(first())
+            .subscribe(
+              (data: any) => {
+                    this.router.navigate([this.returnUrl]);
+                },
+              (error: any) => {
+                    this.alertService.error(error);
+                    this.loading = false;
+                });
+    }
 
 }
